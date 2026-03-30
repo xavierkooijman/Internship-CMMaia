d3e02dc7ddcef252e923df6810fbb23757ce4af1
xavierkooijman <xavierkooijman@gmail.com>
2026-03-30 17:34:37 +0100

    ingestion script for weather data from open meteo

---
+++ scripts/xavier_open_meteo.py
@@ -0,0 +1,304 @@
+#!pip install clts_pcp --quiet
+#!pip install crate --quiet
+#!pip install pymysql --quiet
+#!pip install cryptography --quiet
+import os
+import sys
+import requests
+import json
+import clts_pcp as clts
+import socket
+from datetime import datetime
+
+
+tstart = clts.getts()
+hostname = socket.gethostname()
+
+
+def detect_environment():
+    clts.elapt["Detect Environment"] = clts.deltat(tstart)
+    if "COLAB_RELEASE_TAG" in os.environ:
+        return "colab"
+    elif "RENDER" in os.environ:
+        return "render"
+    elif sys.platform.startswith("win"):
+        return "windows"
+    else:
+        return "linux"
+
+
+env = detect_environment()
+clts.elapt[f"Environment Detected: {env}"] = clts.deltat(tstart)
+print("Running in:", env)
+
+if env == "colab":
+    from google.colab import userdata
+    USER = userdata.get("USER")
+    EMAIL_FROM = userdata.get("EMAIL_FROM")
+    EMAIL_PASSWORD = userdata.get("EMAIL_PASSWORD")
+    DB_LIST = json.loads(userdata.get(f"{USER}-dblist.json"))["databases"]
+    RECEIVERS_LIST = json.loads(userdata.get(
+        f"{USER}-receiverslist.json"))["receivers"]
+
+elif env == "render":
+    USER = os.getenv("USER")
+    EMAIL_FROM = os.getenv("EMAIL_FROM")
+    RESEND_API_KEY = os.getenv("RESEND_API_KEY")
+    DB_LIST = json.load(open(f"/etc/secrets/{USER}-dblist.json"))["databases"]
+    RECEIVERS_LIST = json.load(
+        open(f"/etc/secrets/{USER}-receiverslist.json"))["receivers"]
+
+else:
+    from dotenv import load_dotenv
+    load_dotenv()
+
+    USER = os.getenv("USER")
+    EMAIL_FROM = os.getenv("EMAIL_FROM")
+    EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD")
+    DB_LIST = json.load(open(f"{USER}-dblist.json"))["databases"]
+    RECEIVERS_LIST = json.load(open(f"{USER}-receiverslist.json"))["receivers"]
+
+
+clts.setcontext(
+    f'OpenMeteo Weather Station Data Retrieval - Environment: {env}')
+
+
+data_status = "nok"
+
+url = "https://api.open-meteo.com/v1/forecast?latitude=41.2357&longitude=-8.6199&current_weather=true&timezone=auto"
+
+try:
+    weather_response = requests.get(url)
+    weather_response.raise_for_status()
+    weather_data = weather_response.json()
+    print("Weather data retrieved successfully")
+    clts.elapt[f"Weather Data Retrieved Successfully"] = clts.deltat(
+        tstart)
+    data_status = "ok"
+except Exception as e:
+    print(f"Error during weather data retrieval: {e}")
+    clts.elapt[f"Weather Data Retrieval Failed, Error: {e}"] = clts.deltat(
+        tstart)
+
+print("API Response:", weather_data)
+station_location = "Maia"
+
+
+if data_status == "ok":
+
+    values = (
+        hostname,
+        "OpenMeteo",
+        station_location,
+        weather_data["latitude"],
+        weather_data["longitude"],
+        datetime.fromisoformat(weather_data["current_weather"]["time"]),
+        weather_data["current_weather"]["temperature"],
+        weather_data["current_weather"]["windspeed"],
+        weather_data["current_weather"]["winddirection"]
+    )
+
+    for db in DB_LIST:
+        print(f"Processing database: {db}")
+        status = "nok"
+        clts.elapt[f"Connecting to {db}"] = clts.deltat(tstart)
+
+        try:
+            if env == "render":
+                credentials_path = f"/etc/secrets/{USER}-{db}.json"
+                dbcreds = json.load(open(credentials_path))
+            elif env == "colab":
+                dbcreds = json.loads(userdata.get(f"{USER}-{db}.json"))
+            else:
+                credentials_path = f"secrets/{USER}-{db}.json"
+                dbcreds = json.load(open(credentials_path))
+
+            if dbcreds["dbms"] == "mysql":
+                import pymysql
+                connection = pymysql.connect(
+                    charset="utf8mb4",
+                    connect_timeout=10,
+                    cursorclass=pymysql.cursors.DictCursor,
+                    database=dbcreds["database"],
+                    host=dbcreds["host"],
+                    password=dbcreds["password"],
+                    read_timeout=10,
+                    port=dbcreds["port"],
+                    user=dbcreds["username"],
+                    write_timeout=10,
+                )
+
+                sql = """
+                INSERT INTO open_meteo (
+                    hostfeed,
+                    source,
+                    station_location,
+                    latitude,
+                    longitude,
+                    tstamp,
+                    temperature_c,
+                    windspeed_km_h,
+                    wind_direction_deg
+                ) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s);
+                """
+            elif dbcreds["dbms"] == "tidb":
+
+                import pymysql
+                if env == "render":
+                    CA_PATH = f"/etc/secrets/{dbcreds['ca_path']}"
+                elif env == "colab":
+                    CA_SECRET = userdata.get(f"{dbcreds['ca_path']}")
+                    with open(f"/tmp/{USER}.pem", "w") as f:
+                        f.write(CA_SECRET)
+                    CA_PATH = f"/tmp/{USER}.pem"
+                else:
+                    CA_PATH = f"secrets/{dbcreds['ca_path']}"
+
+                connection = pymysql.connect(
+                    host=dbcreds["host"],
+                    port=dbcreds["port"],
+                    user=dbcreds["username"],
+                    password=dbcreds["password"],
+                    database=dbcreds["database"],
+                    cursorclass=pymysql.cursors.DictCursor,
+                    ssl_verify_cert=True,
+                    ssl_verify_identity=True,
+                    ssl_ca=CA_PATH,
+                )
+
+                sql = """
+                INSERT INTO open_meteo (
+                    hostfeed,
+                    source,
+                    station_location,
+                    latitude,
+                    longitude,
+                    tstamp,
+                    temperature_c,
+                    windspeed_km_h,
+                    wind_direction_deg
+                ) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s);
+                """
+
+            elif dbcreds["dbms"] == "crate":
+                sql = """
+                INSERT INTO open_meteo (
+                    hostfeed,
+                    source,
+                    station_location,
+                    latitude,
+                    longitude,
+                    tstamp,
+                    temperature_c,
+                    windspeed_km_h,
+                    wind_direction_deg
+                ) VALUES (?,?,?,?,?,?,?,?,?);
+                """
+
+                from crate import client
+                connection = client.connect(dbcreds["host"], username=dbcreds["username"],
+                                            password=dbcreds["password"],             verify_ssl_cert=True, timeout=10)
+
+        except Exception as e:
+            print(f"Error for {db}: {e}")
+            clts.elapt[f"Connection to {db} Failed, Error: {e}"] = clts.deltat(
+                tstart)
+            continue
+
+        cursor = connection.cursor()
+        print(f"Connected to {db} successfully")
+        clts.elapt[f"Connection to {db} Successful"] = clts.deltat(tstart)
+        status = "ok"
+
+        try:
+            if status == "ok":
+
+                sql_check_duplicate = """
+                SELECT COUNT(*) AS count FROM open_meteo
+                WHERE station_location = %s AND tstamp = %s
+                """
+
+                values_check_duplicate = (
+                    station_location, datetime.fromisoformat(
+                        weather_data["current_weather"]["time"])
+                )
+
+                if dbcreds["dbms"] == "crate":
+                    sql_check_duplicate = """
+                    SELECT COUNT(*) AS count FROM open_meteo
+                    WHERE station_location = ? AND tstamp = ?
+                    """
+
+                cursor.execute(sql_check_duplicate, values_check_duplicate)
+                result = cursor.fetchone()
+
+                if dbcreds["dbms"] == "crate":
+                    count = result[0]
+                else:
+                    count = result['count']
+
+                if count == 0:
+                    cursor.execute(sql, values)
+                    connection.commit()
+                    print(f"Data inserted into {db} successfully")
+                    clts.elapt[f"Data Inserted into {db} Successfully"] = clts.deltat(
+                        tstart)
+                elif count == 1:
+                    clts.elapt[f"Data for station: {station_location} and timestamp: {weather_data['current_weather']['time']} already exists in {db}, Skipping Insertion"] = clts.deltat(
+                        tstart)
+                else:
+                    clts.elapt[f"Duplicate Count in {db} for station: {station_location} and timestamp: {weather_data['current_weather']['time']}, count: {count}"] = clts.deltat(
+                        tstart)
+
+        except Exception as e:
+            print(f"Error inserting data into {db}: {e}")
+            clts.elapt[f"Data Insertion into {db} Failed, Error: {e}"] = clts.deltat(
+                tstart)
+
+        connection.close()
+        print(f"Connection to {db} closed")
+        clts.elapt[f"Connection to {db} Closed"] = clts.deltat(tstart)
+
+
+toemail = clts.listtimes()
+print(toemail)
+
+if env == "render":
+    import resend
+    resend.api_key = RESEND_API_KEY
+    try:
+        result = resend.Emails.send({
+            "from": "Acme <onboarding@resend.dev>",
+            "to": ["xavierkooijman@gmail.com"],
+            "subject": "Open Meteo Weather Station Data Retrieval Report",
+            "html": toemail,
+        })
+
+        print("Email sent successfully!")
+        print(f"Email ID: {result['id']}")
+    except Exception as e:
+        print(f"Error sending email: {e}")
+        exit(1)
+
+else:
+
+    import smtplib
+    from email.mime.text import MIMEText
+
+    SMTP_SERVER = "smtp.gmail.com"
+    SMTP_PORT = 587
+
+    try:
+        msg = MIMEText(toemail, "html")
+        msg["Subject"] = "Open Meteo Weather Station Data Retrieval Report"
+        msg["From"] = EMAIL_FROM
+        msg["To"] = ", ".join(RECEIVERS_LIST)
+
+        with smtplib.SMTP(SMTP_SERVER, SMTP_PORT) as server:
+            server.starttls()
+            server.login(EMAIL_FROM, EMAIL_PASSWORD)
+            server.sendmail(EMAIL_FROM, RECEIVERS_LIST, msg.as_string())
+
+        print("Email sent!")
+    except Exception as e:
+        print(f"Error sending email: {e}")
